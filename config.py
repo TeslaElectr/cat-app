35b28a928b8fb0f4520fda287ce76446017f238a
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-07 22:15:16 +0300

    fix config

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
-DB_URL = "postgresql+psycopg://user:password@localhost:5432/db_cat"
+# DB_URL = "postgresql+psycopg://user:password@localhost:5432/cat_db",
+DB_URL = "postgresql+psycopg://user:password@localhost:5432/catdb"
 SECRET_KEY = "72970e69eeee063cd7c706736d861759e5e395751fae80dc89a71a5991b3115c"
