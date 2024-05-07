85791de0e3932dcff2f5a1cac90e93a158ef3cc7
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-07 19:23:39 +0300

    add config data

---
+++ config.py
@@ -0,0 +1,2 @@
+DB_URL = "postgresql+psycopg://user:password@localhost:5432/db_cat"
+SECRET_KEY = "72970e69eeee063cd7c706736d861759e5e395751fae80dc89a71a5991b3115c"
--- main.py
+++ main.py
@@ -2,12 +2,25 @@ from flask import Flask
 
 from views import root_app
 from views import cat_app
+from models import db
+
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
 
 app.register_blueprint(root_app)
 app.register_blueprint(cat_app)
+
+app.config(
+    SQLALCHEMY_DATABASE_URI="",
+    SECRET_KEY="",
+)
+
+db.init_app(app=app)
+migrate = Migrate(app=app, db=db)
+
+
 
 
 
