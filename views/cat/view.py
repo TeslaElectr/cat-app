32c9a5e67687828470eba9700687b96daaa254f5
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-07 23:57:24 +0300

    fix view cat cherry-pick

--- views/cat/view.py
+++ views/cat/view.py
@@ -1,4 +1,9 @@
 from flask import Blueprint
+from flask import request
+from . import crud
+
+from models import Phrase
+from cat_api import get_random_cats_fact
 
 cat_app = Blueprint(
     "cat_app",
@@ -7,6 +12,25 @@ cat_app = Blueprint(
 )
 
 
-@cat_app.route("/")
-def get_cats():
-    return {"cats": "here"}
+@cat_app.route("/phrases/", methods=["GET"])
+def get_all_phrases() -> list[Phrase]:
+    return crud.get_list_of_phrases()
+
+    
+    
+@cat_app.route("/phrase/<int:phrase_id>/", methods=["GET"])
+def get_phrase_by_id(phrase_id: int) -> Phrase:
+    return crud.get_phrase_by_id(phrase_id=phrase_id)
+
+    
+    
+@cat_app.route("/create-phrase/", methods=["GET"])
+def create_phrase_fact():
+    fact = get_random_cats_fact()
+    new_frase = crud.create_phrase(
+        phras=fact,
+        )
+    return crud.get_phrase_by_id(phrase_id=new_frase.id)
+
+
+    
