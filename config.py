dc72733281b033dadf5b2bb7700a8b9f178cb7c1
Ivan Lebedev <ivanleb171@gmail.com>
2024-05-19 22:05:20 +0300

    add dockerignore

--- config.py
+++ config.py
@@ -1,3 +1,21 @@
-# DB_URL = "postgresql+psycopg://user:password@localhost:5432/cat_db",
-DB_URL = "postgresql+psycopg://user:password@localhost:5432/catdb"
-SECRET_KEY = "72970e69eeee063cd7c706736d861759e5e395751fae80dc89a71a5991b3115c"
+# DB_URL = "postgresql+psycopg://user:password@localhost:5432/catdb"
+# SECRET_KEY = "72970e69eeee063cd7c706736d861759e5e395751fae80dc89a71a5991b3115c"
+
+from os import getenv
+
+SQLALCHEMY_DATABASE_URI = getenv(
+    "SQLALCHEMY_DATABASE_URI",
+    "postgresql+psycopg://user:password@localhost:5432/catdb",
+)
+
+
+class Config:
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI
+    SECRET_KEY = "" 
+    
+
+class DevelopmentConfig(Config):
+    DEBUG = False
+    TESTING = False
+    SECRET_KEY = "b65a50a2fdb48bbae1352af577ac27765ff65167631267b3e1a3be302917aa7c"
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import getenv
 from flask_migrate import Migrate
 from flask import Flask
 
@@ -16,14 +17,24 @@ app.register_blueprint(root_app)
 app.register_blueprint(ph_app)
 app.register_blueprint(cat_app)
 
-app.config.update(
-    SQLALCHEMY_DATABASE_URI=config.DB_URL,
-    SECRET_KEY=config.SECRET_KEY,
+# app.config.update(
+#     SQLALCHEMY_DATABASE_URI=config.DB_URL,
+#     SECRET_KEY=config.SECRET_KEY,
+# )
+
+CONFIG_NAME = getenv(
+    "CONFIG_NAME",
+    "DevelopmentConfig",
 )
+
+app.config.from_object(f"config.{CONFIG_NAME}")
 
 db.init_app(app=app)
 migrate = Migrate(app=app, db=db)
 
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 
